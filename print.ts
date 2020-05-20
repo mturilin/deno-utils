2ac4621a18479f4fb9535e9ab8b75e12d49757c4
Mikhail Turilin <mturilin@gmail.com>
2020-05-20 15:29:15 -0700

    print.ts

---
+++ print.ts
@@ -0,0 +1,11 @@
+import { encode } from "https://deno.land/std/encoding/utf8.ts"
+
+export function print(...args: string[]) {
+    let out_str = args.join(" ");
+    Deno.stdout.write(encode(out_str));
+}
+
+export function println(...args: string[]) {
+    print(...args);
+    print('\n');
+}
