513f0565755618793eb5306aadde998c26601a9d
Mikhail Turilin <mturilin@gmail.com>
2020-05-20 15:39:27 -0700

    Actually that was a stupid idea. Made them sync.

--- print.ts
+++ print.ts
@@ -1,11 +1,11 @@
 import { encode } from "https://deno.land/std/encoding/utf8.ts"
 
-export async function print(...args: string[]) {
+export function print(...args: string[]) {
     let out_str = args.join(" ");
-    await Deno.stdout.write(encode(out_str));
+    Deno.stdout.writeSync(encode(out_str));
 }
 
-export async function println(...args: string[]) {
-    await print(...args);
-    await print('\n');
+export function println(...args: string[]) {
+    print(...args);
+    print('\n');
 }
